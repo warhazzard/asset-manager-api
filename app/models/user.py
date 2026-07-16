f7aa21f40a1145e25cdf2456e5e2e9fb10fc2bc8
warhazzard <peeyush_j@ce.iitr.ac.in>
2026-07-16 17:41:02 +0530

    fix: finalize user model

--- app/models/user.py
+++ app/models/user.py
@@ -14,7 +14,7 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     firstname = Column(String(50), nullable=False)
     lastname = Column(String(50), nullable=False)
-    email = Column(String(100), unique=True, nullable=False)
+    email = Column(String(100), unique=True, index=True, nullable=False)
     hashed_password = Column(String(60), nullable=False)
     role = Column(SQLEnum(UserRole), nullable=False, default=UserRole.EMPLOYEE)
     is_active = Column(Boolean, default=True)
